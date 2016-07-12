e5ecf27f6fb0443b58f167b9e29f74e1b4b9af75
Hack Reactor Students <accounts@hackreactor.com>
2016-07-11 21:31:09 -0700

    Successfully added Ness to the Heroes table.

---
+++ server/db/schema.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var enemiesSchema = new Schema({
+  id: Number,
+  name: String,
+  hp: Number,
+  atk: Number,
+  def: Number,
+  exp: Number,
+  money: Number,
+  img: String
+});
+
+var heroesSchema = new Schema({
+  name: String,
+  level: Number,
+  hp: Number,
+  atk: Number,
+  def: Number,
+  exp: Number,
+  expToGo: Number,
+  money: Number
+});
+
+var Enemies = mongoose.model('Enemies', enemiesSchema);
+var Heroes = mongoose.model('Heroes', heroesSchema);
+
+module.exports = {
+  Enemies: Enemies,
+  Heroes: Heroes
+}
--- server/server.js
+++ server/server.js
@@ -4,30 +4,61 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var tables = require('./db/schema');
 
-// Config files
-var db = require('./config/db-config');
-
-// Set up port
-var port = process.env.PORT || 8080;
 
 // Connect to mongoDB
-mongoose.connect(db.url);  
+mongoose.connect('mongodb://localhost/earthbound');
 
 // Get all data of the body (POST) parameters parse application/json 
 app.use(bodyParser.json()); 
 
-app.use(express.static(__dirname + "/../client/lib"));
 app.use(express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../client/lib"));
+
 
 // Configure our routes
 // require('./app/routes')(app);
 
+// Set up port
+var port = process.env.PORT || 8080;
+
 // Start app at http://localhost:8080
 app.listen(port, function () {
-  console.log(`Listening on port ${port}!`);
+  console.log(`The party is getting started on port ${port}!`);
 });
 
-// expose app           
-// exports = module.exports = app;
+
+/**************************
+  Insert data into tables
+**************************/
+
+// var ness = new tables.Heroes({
+//   name: 'Ness',
+//   level: 1,
+//   hp: 100,
+//   atk: 25,
+//   def: 10,
+//   exp: 0,
+//   expToGo: 500,
+//   money: 0
+// });
+
+// ness.save(function(err, data) {
+//   if (err) {
+//     console.log('Error adding data...', err);
+//   } else {
+//     console.log('Successfully add data', data);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
 module.exports = app;
