ea3e3dfb8b78b694ae536ef3fabc0b094fbc9552
Hack Reactor Students <accounts@hackreactor.com>
2016-07-11 22:02:50 -0700

    Inserted 5 monsters into the enemies table.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var tables = require('./db/schema');
 
-
 // Connect to mongoDB
 mongoose.connect('mongodb://localhost/earthbound');
 
@@ -15,7 +14,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/../client"));
 app.use(express.static(__dirname + "/../client/lib"));
-
 
 // Configure our routes
 // require('./app/routes')(app);
@@ -28,10 +26,20 @@ app.listen(port, function () {
   console.log(`The party is getting started on port ${port}!`);
 });
 
+// Save to DB helper function
+var save = function(char) {
+  char.save(function(err, data) {
+    if (err) {
+      console.log('Error adding data...', err);
+    } else {
+      console.log('Successfully add data', data);
+    }
+  });
+};
 
-/**************************
-  Insert data into tables
-**************************/
+/****************************************************
+  Insert into heroes table
+****************************************************/
 
 // var ness = new tables.Heroes({
 //   name: 'Ness',
@@ -44,20 +52,81 @@ app.listen(port, function () {
 //   money: 0
 // });
 
-// ness.save(function(err, data) {
-//   if (err) {
-//     console.log('Error adding data...', err);
-//   } else {
-//     console.log('Successfully add data', data);
-//   }
+// save(ness);
+
+// // Select from DB example
+var ness = tables.Heroes.findOne({'name': 'Ness'});
+
+console.log(ness);
+
+/****************************************************
+  Insert into enemies table
+****************************************************/
+
+// var zombieDog = new tables.Enemies({
+//   id: 1,
+//   name: 'Zombie Dog',
+//   hp: 210,
+//   atk: 39,
+//   def: 51,
+//   exp: 1354,
+//   money: 54,
+//   img: 'http://walkthrough.starmen.net/earthbound/image/enemies/zombiedog.png'
 // });
 
+// save(zombieDog);
 
+// var urbanZombie = new tables.Enemies({
+//   id: 2,
+//   name: 'Urban Zombie',
+//   hp: 171,
+//   atk: 31,
+//   def: 24,
+//   exp: 700,
+//   money: 58,
+//   img: 'http://walkthrough.starmen.net/earthbound/image/enemies/urbanzombie.png'
+// });
 
+// save(urbanZombie);
 
+// var coilSnake = new tables.Enemies({
+//   id: 3,
+//   name: 'Thirsty Coil Snake',
+//   hp: 270,
+//   atk: 52,
+//   def: 80,
+//   exp: 2786,
+//   money: 276,
+//   img: 'http://walkthrough.starmen.net/earthbound/image/enemies/thirstycoilsnake.png'
+// });
 
+// save(coilSnake);
 
+// var starman = new tables.Enemies({
+//   id: 4,
+//   name: 'Starman',
+//   hp: 545,
+//   atk: 103,
+//   def: 126,
+//   exp: 23396,
+//   money: 720,
+//   img: 'http://walkthrough.starmen.net/earthbound/image/enemies/starman.png'
+// });
 
+// save(starman);
+
+// var guardianDigger = new tables.Enemies({
+//   id: 5,
+//   name: 'Guardian Digger',
+//   hp: 386,
+//   atk: 59,
+//   def: 129,
+//   exp: 17301,
+//   money: 1467,
+//   img: 'http://walkthrough.starmen.net/earthbound/image/enemies/guardiandigger.png'
+// });
+
+// save(guardianDigger);
 
 
 
